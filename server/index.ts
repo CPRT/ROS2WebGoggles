b1ef2c412021c6ffc2421ab54060a8d5aa24fd0b
mateusz <tycmat9@gmail.com>
2024-04-04 19:09:48 -0400

    Refactor server code and add TypeScript support

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+
