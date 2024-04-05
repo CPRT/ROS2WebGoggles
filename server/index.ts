21f1602c40765a6df9ffafe27b37e79e2d415665
mateusz <tycmat9@gmail.com>
2024-04-04 21:52:22 -0400

    Refactor server code to serve static files and add TypeScript support

--- server/index.ts
+++ server/index.ts
@@ -4,15 +4,10 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
