154e817688068cec9e0b22a1a906a6e7eae75b82
mateusz <tycmat9@gmail.com>
2024-04-06 21:33:06 -0400

    Add StringMsgBox component for displaying string messages

---
+++ client/src/components/StringMsgBox.tsx
@@ -0,0 +1,8 @@
+export default function StringMessageBox() {
+    return (
+        <section>
+            <h1>String Message Box</h1>
+            <p>String message box is a simple React component that fetches a string from the server and displays it in a message box.</p>
+        </section>
+    );
+}
