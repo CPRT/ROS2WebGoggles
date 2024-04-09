33abcf12e61c7ce8bebacc4e1564fa5539a7a2b7
lead <softlead@curovers.ca>
2024-04-09 18:51:18 -0400

    Update nginx.conf and App.tsx to enable proxying to /api endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,11 @@ import "./App.css";
 
 function App() {
   const [response, setResponse] = useState("");
+  const [topics, setTopics] = useState([]);
 
   useEffect(() => {
     var io = require('socket.io-client');
-    const socket = io('http://192.168.2.10', {
+    const socket = io('http://192.168.55.1', {
       path: '/mysock'
     });
     socket.on("rosMsg", (data: React.SetStateAction<string>) => {
@@ -21,11 +22,23 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    fetch("http://192.168.55.1/api/topics")
+      .then((res) => res.json())
+      .then((data) => setTopics(data.topics.map((topic: string) => ({ id: topic, name: topic }))));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         Current ROS msg: {response}
       </header>
+      <h2>Available topics</h2>
+      <ul>
+      {topics.map((item: any) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+      </ul>
     </div>
   );
 }
